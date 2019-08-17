41cf40e4c14d76ce0621466e792715dfe84908f8
Corey Hawkins <corey@Coreys-MacBook-Air.local>
2019-08-16 18:54:04 -0700

    SEMICOLONS

--- promises.js
+++ promises.js
@@ -1,5 +1,5 @@
 var newUserObj;
-var indexI = newUserObj
+var indexI = newUserObj;
 
 let guessValue;
 let getUsers = () => {
@@ -8,7 +8,7 @@ let getUsers = () => {
   $.get("http://jsonplaceholder.typicode.com/users", (data) => {
     let newUserObj = data.find((user) => {
       return user.username === guessValue;
-    })
+    });
     var brettsID = "";
     $.get('http://jsonplaceholder.typicode.com/users', function(data) {
       brettsID = data[newUserObj.id - 1].username;
@@ -16,7 +16,7 @@ let getUsers = () => {
       let userName = document.getElementById("userName");
 
       if (guessValue == brettsID) {
-        userName.innerHTML = data[newUserObj.id - 1].username
+        userName.innerHTML = data[newUserObj.id - 1].username;
       } else {
         userName.innerHTML = 'Wrong User';
       }
@@ -28,27 +28,27 @@ let getUsers = () => {
           throw ('New exception');
         }
       } catch (err) {
-        userName.innerHTML = 'Wrong User LOOK IM AN ERROR MESSAGE!'
+        userName.innerHTML = 'Wrong User LOOK IM AN ERROR MESSAGE!';
       };
     }
-    please()
-    let userEl = document.getElementById("name")
+    please();
+    let userEl = document.getElementById("name");
     $.get('http://jsonplaceholder.typicode.com/users', function(data) {
       console.log(data[newUserObj.id - 1].name);
-      userEl.innerHTML = data[newUserObj.id - 1].name
+      userEl.innerHTML = data[newUserObj.id - 1].name;
     });
 
-    let albumEl = document.getElementById("albumTitle")
+    let albumEl = document.getElementById("albumTitle");
     $.get('http://jsonplaceholder.typicode.com/albums', function(data) {
       console.log(data[newUserObj.id - 1].title);
-      albumEl.innerHTML = data[newUserObj.id - 1].title
+      albumEl.innerHTML = data[newUserObj.id - 1].title;
     });
 
-    let postEl = document.getElementById("postTitle")
+    let postEl = document.getElementById("postTitle");
     $.get('http://jsonplaceholder.typicode.com/posts', function(data) {
       console.log(data[newUserObj.id - 1].title);
-      postEl.innerHTML = data[newUserObj.id - 1].title
+      postEl.innerHTML = data[newUserObj.id - 1].title;
     });
-    console.log(newUserObj.id)
-  })
+    console.log(newUserObj.id);
+  });
 };
